1331af5923cce43fa58321f903e25bc89f4dc880
hx-khan <HK.32@outlook.com>
2023-11-01 20:01:22 +0100

    added spring boot

---
+++ stockmark/src/main/java/stockmark/stockmark/StockmarkApplication.java
@@ -0,0 +1,13 @@
+package stockmark.stockmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockmarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockmarkApplication.class, args);
+	}
+
+}
---
+++ stockmark/src/test/java/stockmark/stockmark/StockmarkApplicationTests.java
@@ -0,0 +1,13 @@
+package stockmark.stockmark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockmarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
