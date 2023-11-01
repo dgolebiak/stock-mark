7582722de3f924eca8ac09e6df1764d0f9d7b5a3
David Golebiak <davidgolebiak@gmail.com>
2023-11-01 17:00:38 +0100

    Account changes
    Changed the stocks arraylist to hashmap and added some basic functions

--- logic/Account.java
+++ logic/Account.java
@@ -1,6 +1,27 @@
+import java.util.ArrayList;
+
 // Single account for one user
 public class Account {
     private double riksdaler;
     private String name;
-    private ArrayList<Stock> stocks;
+    private Map<Stock, Integer> ownedStocks;
+
+    public String getName() {
+        return name;
+    }
+    public void deductRiksdaler(double riksdaler) {
+        this.riksdaler -= riksdaler;
+    }
+    public void addRiksdaler(double riksdaler) {
+        this.riksdaler += riksdaler;
+    }
+    public double getRiksdaler() {
+        return riksdaler;
+    }
+    public void addStock(Stock stock, int quantity) {
+        stocks.put(stock, quantity);
+    }
+    public void removeStock(Stock stock, int quantity) {
+        stocks.put(stock, stocks.get(stock) - quantity);
+    }
 }
