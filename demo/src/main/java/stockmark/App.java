e6558044439710885d957eccae28e635dc8eba31
hx-khan <HK.32@outlook.com>
2023-11-01 15:52:30 +0100

    Initial

---
+++ demo/src/main/java/stockmark/App.java
@@ -0,0 +1,11 @@
+package stockmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
