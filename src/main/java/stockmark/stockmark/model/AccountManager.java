18d4ba670f0142fbeed67413e1e7ad04c520407f
Elias Nilsson <elinil0221@gmail.com>
2023-11-12 21:06:18 +0100

    login and hashmap

--- src/main/java/stockmark/stockmark/model/AccountManager.java
+++ src/main/java/stockmark/stockmark/model/AccountManager.java
@@ -2,26 +2,29 @@ package stockmark.stockmark.model;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import stockmark.stockmark.model.Account;
-import java.util.ArrayList;
-
-import java.io.EOFException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
 
 public class AccountManager {
     
-    private ArrayList<Account> accounts; // Will change to a hashmap in the future.
+    private HashMap<String, Account> accounts;
 
     public AccountManager() {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
-            accounts = objectMapper.readValue(new File("src/main/resources/accounts.json"), new TypeReference<ArrayList<Account>>(){});
+            List<Account> accountsList = objectMapper.readValue(
+                new File("src/main/resources/accounts.json"), 
+                new TypeReference<List<Account>>(){}
+            );
+            accounts = new HashMap<>();
+            for (Account account : accountsList) {
+                accounts.put(account.getEmail(), account);
+            }
         } catch (FileNotFoundException e) {
             System.out.println("No accounts file found.");
-        } catch (EOFException e) {
-            System.out.println("Accounts file is empty.");
         } catch (IOException e) {
             e.printStackTrace();
             System.out.println("Error reading accounts file.");
@@ -29,23 +32,45 @@ public class AccountManager {
     }
 
     public void createAccount(String email, String password) {
+        if (accounts.containsKey(email)) {
+            System.out.println("Account with email already exists.");
+            return;
+        }
         Account account = new Account(email, password);
-        accounts.add(account);
+        accounts.put(email, account);
         saveAccountsJson();
+        //return html
+    }
+
+    public void loginAccount(String email, String password) {
+        if (!accounts.containsKey(email)) {
+            System.out.println("Account with this email does not exist.");
+            return;
+        }
+        Account account = accounts.get(email);
+    
+        if (account.getPassword().equals(password)) {
+            System.out.println("Login successful!");
+            return; //return html
+        } else {
+            System.out.println("Incorrect password."); //raise exception
+        }
     }
 
     public void saveAccountsJson() {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
-            objectMapper.writeValue(new File("src/main/resources/accounts.json"), accounts);
+            objectMapper.writeValue(
+                new File("src/main/resources/accounts.json"), 
+                accounts.values()
+            );
         } catch (IOException e) {
             e.printStackTrace();
             System.out.println("Error saving accounts.");
         }
     }
 
-    public ArrayList<Account> getAccounts() {
+    public HashMap<String, Account> getAccounts() {
         return accounts;
     }
-
 }
