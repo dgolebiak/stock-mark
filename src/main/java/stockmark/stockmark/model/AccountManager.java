253425258180fe268879577df48593cd4a7f55aa
hx-khan <HK.32@outlook.com>
2023-11-23 03:28:24 +0100

    sync login/register functions

--- src/main/java/stockmark/stockmark/model/AccountManager.java
+++ src/main/java/stockmark/stockmark/model/AccountManager.java
@@ -6,10 +6,12 @@ import stockmark.stockmark.model.Exceptions.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.UUID;
 
 public class AccountManager {
     private static String accountsFile = "./src/main/resources/accounts.json";
     private static HashMap<String, Account> accounts;
+    private static HashMap<UUID, String> loggedIn = new HashMap<UUID, String>();
 
     public static void Initialize() {
         if (accounts != null) return;
@@ -26,20 +28,33 @@ public class AccountManager {
         }
     }
 
-    public static void registerAccount(Account acc) throws AccountExistsException {
+    public static UUID registerAccount(Account acc) throws AccountExistsException {
         if (accounts.containsKey(acc.getEmail())) {
             throw new AccountExistsException();
         }
         accounts.put(acc.getEmail(), acc);
         saveAccountsJson();
+        UUID id = java.util.UUID.randomUUID();
+        loggedIn.put(id, acc.getEmail());
+        return id;
     }
 
-    public static Account loginAccount(String email, String password) throws AccountNotFoundException, IncorrectPasswordException {
+    public static UUID loginAccount(String email, String password) throws AccountNotFoundException, IncorrectPasswordException {
         if (!accounts.containsKey(email)) throw new AccountNotFoundException();
 
         Account account = accounts.get(email);
         if (!account.getPassword().equals(password)) throw new IncorrectPasswordException();
-        return account;
+        UUID id = java.util.UUID.randomUUID();
+        loggedIn.put(id, email);
+        return id;
+    }
+
+    public static boolean isLoggedIn(UUID id) {
+        return loggedIn.containsKey(id);
+    }
+
+    public static Account getFromUUID(UUID id) {
+        return accounts.get(loggedIn.get(id));
     }
 
     public static void saveAccountsJson() {
