58491b1a325bcd7f58600aef8bddba08237a61dc
hx-khan <HK.32@outlook.com>
2023-11-24 21:19:34 +0100

    cleanup

--- src/main/java/stockmark/stockmark/web/PortfolioController.java
+++ src/main/java/stockmark/stockmark/web/PortfolioController.java
@@ -1,19 +1,27 @@
 package stockmark.stockmark.web;
 
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import stockmark.stockmark.model.Account;
 import stockmark.stockmark.model.AccountManager;
 
 // @CookieValue(value = "uuid", defaultValue = "") String uuid
 @Controller
 public class PortfolioController {
     @GetMapping("/portfolio")
-    public String onGetPortfolio(@CookieValue(value = "uuid", defaultValue = "") String uuid) {
+    public String onGetPortfolio(@CookieValue(value = "uuid", defaultValue = "") String uuid, Model model) {
+        // if not logged in; redirect to login
         if (uuid.equals("") || !AccountManager.isLoggedIn(java.util.UUID.fromString(uuid)))
             return "redirect:/";
 
+        Account acc = AccountManager.getFromUUID(java.util.UUID.fromString(uuid));
+
+        // not complete
+        model.addAttribute("assets", acc.getAssets());
+        // render portfolio template
         return "portfolio";
     }
 }
