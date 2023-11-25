d61680202799bb5383952f20911d01c57009f40a
hx-khan <HK.32@outlook.com>
2023-11-25 02:29:56 +0100

    small clarifications

--- src/main/java/stockmark/stockmark/model/Share.java
+++ src/main/java/stockmark/stockmark/model/Share.java
@@ -1,4 +1,4 @@
 package stockmark.stockmark.model;
 
-// Shares("TSLA", 4, 235) -- buyPrice is an average
+// Example: Share(4, 235) -- buyPrice is an average
 public record Share(int amount, double buyPrice){}
--- src/main/java/stockmark/stockmark/model/Transaction.java
+++ src/main/java/stockmark/stockmark/model/Transaction.java
@@ -1,3 +1,4 @@
 package stockmark.stockmark.model;
 
+// Example: Transaction("buy", "TSLA", 4, 235, "2023-11-25")
 public record Transaction(String action, String ticker, int amount, double unitPrice, String time){}
